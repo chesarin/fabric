1d823aded4de4847c307d38f3a3c989edac1d5f6
Cesar Vargas <punisher@lanegra-linux>
2014-06-10 14:00:27 -0400

    remotehosts scripts to upgrade packages

---
+++ remotehosts.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import getpass
+from cuisine import *
+from fabric.api import *
+
+def read_hosts_file(filename=None):
+    if not filename:
+        filename = raw_input("Enter filename to read:")
+    myhosts = []
+    with open(filename,"r") as file:
+        for host in file:
+            myhosts.append(host)
+    print 'Total number of entries in myhost is {}'.format(len(myhosts))
+    return myhosts
+
+env.hosts = read_hosts_file()
+
+
+def update_package():
+    password = getpass.getpass(prompt="Enter your sudo password:> ")
+    sudo_password(password)
+    select_package("yum")
+    package = raw_input("What package would you like to upgrade:> ")
+    upgrade = raw_input("Upgrading package {} should we continue Y/N?".format(package))
+    package_ensure(package)
+    if upgrade == "Y":
+        package_update_yum(package)
+    elif upgrade == "N":
+        print "We are terminating without upgrading"
+    else:
+        print "I do not understand option {}".format(upgrade)
